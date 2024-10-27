64af32f39175ff0d82eb813883cee6ee981567c3
Paolo Celada <paolo.cela@gmail.com>
2024-10-27 12:34:11 +0100

    migliorato titolo gp in championship

--- src/app/views/championship/championship.component.ts
+++ src/app/views/championship/championship.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { DocsExampleComponent } from '@docs-components/public-api';
-import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective } from '@coreui/angular';
+import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective, ContainerComponent } from '@coreui/angular';
 import { cifMc, cifSg, cifIt, cifUs } from '@coreui/icons';
 import { IconDirective } from '@coreui/icons-angular';
 
@@ -23,7 +23,7 @@ interface AllGPs {
 @Component({
   selector: 'app-championship',
   standalone: true,
-  imports: [IconDirective, CommonModule, IconDirective, RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, DocsExampleComponent, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective],
+  imports: [ContainerComponent, IconDirective, CommonModule, IconDirective, RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, DocsExampleComponent, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective],
   templateUrl: './championship.component.html',
   styleUrl: './championship.component.scss'
 })
