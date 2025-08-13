1298b072f36ce19a1f1ada4e1d60dce92e48f969
AndreaDominici <dominiciandrea98@gmail.com>
2025-08-14 00:22:44 +0200

    feat(login): add user icon fallback in dropdown and update imports

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FormModule } from '@coreui/angular';
 import { AuthService } from './../../service/auth.service';
-import {   AvatarComponent, DropdownCloseDirective, DropdownComponent, DropdownItemDirective, DropdownMenuDirective, DropdownToggleDirective, GridModule, ButtonDirective, ModalComponent, ModalHeaderComponent, ModalBodyComponent, ModalFooterComponent, ModalToggleDirective} from '@coreui/angular';
+import {   AvatarComponent, DropdownComponent, DropdownItemDirective, DropdownMenuDirective, DropdownToggleDirective, GridModule, ButtonDirective, ModalComponent, ModalHeaderComponent, ModalBodyComponent, ModalFooterComponent, ModalToggleDirective} from '@coreui/angular';
 import { DbDataService } from '../../../app/service/db-data.service';
 import { cilWarning, cilAccountLogout } from '@coreui/icons';
 import { IconDirective } from '@coreui/icons-angular';
+import { cilUser } from '@coreui/icons';
 
 @Component({
     selector: 'login-component',
@@ -34,6 +35,8 @@ import { IconDirective } from '@coreui/icons-angular';
 })
 
 export class LoginComponent {
+
+  icons = { cilUser };
   // Current user id
   loggedUserId: string = 'default';
 
