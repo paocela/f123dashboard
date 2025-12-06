c53653e5e1dfa841f333073455bb88abc7eafed2
AndreaDominici <dominiciandrea98@gmail.com>
2025-12-06 13:09:59 +0100

    fix: specify path for loading environment variables in server configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ import logger, { stream } from './config/logger.js';
 import { EmailService } from './services/mail.service.js';
 
 // Load environment variables
-dotenv.config();
+process.env.NODE_ENV === 'production' ? dotenv.config({path: '/var/www/rff/f123dashboard/.env'}): dotenv.config();
 // Get current directory in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
